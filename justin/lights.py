cdfe43577d29b35b3060cdee10869f078b0d52d3
harbinger1080 <harbinger1080@penguin>
2020-08-29 14:35:37 -0400

    stay on

--- justin/lights.py
+++ justin/lights.py
@@ -11,8 +11,9 @@ def set_pixel_hsv(x, y, h, s, v):
     r, g, b = [int(x*255) for x in col.hsv_to_rgb(h, s, v)]
     uh.set_pixel(x,y,r,g,b)
 
-for y in range(4):
-    for x in range(8):
-        #print("x{}, y{}".format(x,y))
-        set_pixel_hsv(x,y,(1.0/8)*x,(1.0/4)*y,1.0)
-        uh.show()
+while True:
+    for y in range(4):
+        for x in range(8):
+            #print("x{}, y{}".format(x,y))
+            set_pixel_hsv(x,y,(1.0/8)*x,(1.0/4)*y,1.0)
+            uh.show()
