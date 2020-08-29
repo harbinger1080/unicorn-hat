796c02bb4c5b7e0e627e46a30c8f601341fb5289
harbinger1080 <harbinger1080@penguin>
2020-08-29 15:01:26 -0400

    tick tock

--- justin/lights.py
+++ justin/lights.py
@@ -7,6 +7,7 @@ uh.rotation(0)
 uh.brightness(0.25)
 width,height=uh.get_shape()
 
+columns = 0
 t = 15
 
 def set_pixel_hsv(x, y, h, s, v):
@@ -14,16 +15,14 @@ def set_pixel_hsv(x, y, h, s, v):
     uh.set_pixel(x,y,r,g,b)
 
 while (t):
+    columns = t % 15
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
-    time.sleep(1)
-    t -= 1
-
-    columns = t % 15
-
 
     for y in range(height):
         for x in range(columns):
             uh.set_pixel(x,y,3, 130, 255)
             uh.show()
+    time.sleep(1)
+    t -= 1
