b1124a69576beadd5bd3a5c50ac74a7b451a3761
harbinger1080 <harbinger1080@penguin>
2020-08-29 14:43:00 -0400

    timer

--- justin/lights.py
+++ justin/lights.py
@@ -1,5 +1,6 @@
 import colorsys as col
 import unicornhat as uh
+import time
 #print(col.hsv_to_rgb(1.0,1.0,1.0))
 
 uh.set_layout(uh.AUTO)
@@ -10,6 +11,14 @@ width,height=uh.get_shape()
 def set_pixel_hsv(x, y, h, s, v):
     r, g, b = [int(x*255) for x in col.hsv_to_rgb(h, s, v)]
     uh.set_pixel(x,y,r,g,b)
+
+def countdown(t):
+    while(t):
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins,secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
 
 while True:
     for y in range(height):
