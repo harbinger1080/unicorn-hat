cbb87f2c187d61b2bb25ba5e99249a89e17b14bb
harbinger1080 <harbinger1080@penguin>
2020-08-29 13:57:29 -0400

    from scratch

---
+++ justin/lights.py
@@ -0,0 +1,16 @@
+import colorsys as col
+import unicornhat as uh
+#print(col.hsv_to_rgb(1.0,1.0,1.0))
+
+uh.set_layout(uh.AUTO)
+uh.rotation(0)
+uh.brightness(0.25)
+width,height=uh.get_shape()
+
+def set_pixel_hsv(x, y, h, s, v):
+    r, g, b = [int(x*255) for x in col.hsv_to_rgb(h, s, v)]
+    uh.set_pixel(x,y,r,g,b)
+
+for y in range(height):
+    for x in range(width):
+        print((1.0/width))
