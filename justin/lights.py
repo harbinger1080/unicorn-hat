776e5a93e882690641f4697b86bed6b082b31faa
harbinger1080 <harbinger1080@penguin>
2020-08-29 17:07:28 -0400

    testing coords math

--- justin/lights.py
+++ justin/lights.py
@@ -1,27 +1,27 @@
 import colorsys as col
 import unicornhat as uh
 import time
+import math
 
 uh.set_layout(uh.AUTO)
 uh.rotation(0)
 uh.brightness(0.25)
 
 t = int(input("How many minutes?"))
+i=0
+def dcounter(totaldots):
+    width = (math.ceil(totaldots/4))
+    for x in range(width):
+        y = (totaldots - x * 4)
+        if (y > 4):
+            y = 4
+        print(x, y)
+        #uh.set_pixel(x,i,3, 130, 255)
+        #uh.show()
+        time.sleep(1)
 
 while (t):
     mins, secs = divmod(t, 60)
-    dots = (t*60)//225
-
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
-
-    for x in range(dots//4):
-        if (dots % 4 == 0):
-            dheight = 4
-        else:
-            dheight = dots
-        for y in range(dheight):
-            uh.set_pixel(x,y,3, 130, 255)
-            uh.show()
-        time.sleep(1)
     t -= 1
