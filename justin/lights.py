f88151e0b0a983756a255aa6a39286ac152a0add
harbinger1080 <harbinger1080@penguin>
2020-08-29 16:21:51 -0400

    getting the X coords right

--- justin/lights.py
+++ justin/lights.py
@@ -7,20 +7,18 @@ uh.rotation(0)
 uh.brightness(0.25)
 width,height=uh.get_shape()
 
-columns = 0
 t = 90
 
 while (t):
     dots = (t * 60)//225
     print (dots)
     mins, secs = divmod(t, 60)
-
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
 
     for dot in range(dots):
         for y in range(height):
-            for x in range(columns):
+            for x in range(width):
                 uh.set_pixel(x,y,3, 130, 255)
                 uh.show()
         time.sleep(1)
