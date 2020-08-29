5cc1098e64f82e914a03cb4cb6a99c85ad9f1187
harbinger1080 <harbinger1080@penguin>
2020-08-29 17:47:47 -0400

    debug...

--- justin/lights.py
+++ justin/lights.py
@@ -23,7 +23,7 @@ def dcounter(totaldots):
 while (t):
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
-    #print(timeformat, end='\r')
+    print(timeformat, end='\r')
     dcounter(t//25)
     time.sleep(1)
     t -= 1
