ee22e1d96d7793cbe3daa34b71f45f7c223a0d4c
harbinger1080 <harbinger1080@penguin>
2020-08-29 17:30:33 -0400

    fixing dot counts

--- justin/lights.py
+++ justin/lights.py
@@ -26,6 +26,6 @@ while (t):
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
-    dcounter(t)
+    dcounter(mins//3.75)
     time.sleep(1)
     t -= 1
