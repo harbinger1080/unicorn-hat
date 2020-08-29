d6da464757e5f785c80318cb69018c03b3732e0d
harbinger1080 <harbinger1080@penguin>
2020-08-29 16:36:48 -0400

    yikes loops

--- justin/lights.py
+++ justin/lights.py
@@ -5,20 +5,23 @@ import time
 uh.set_layout(uh.AUTO)
 uh.rotation(0)
 uh.brightness(0.25)
-width,height=uh.get_shape()
 
 t = int(input("How many minutes?"))
 
 while (t):
-    dots = (t * 60)//225
-    print (dots)
     mins, secs = divmod(t, 60)
+    dots = (t*60)//225
+
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
 
-    for y in range(height):
-        for x in range(dots//4):
+    for x in range(dots//4):
+        if (dots % 4 == 0):
+            dheight = 4
+        else:
+            dheight = dots
+        for y in range(dheight):
             uh.set_pixel(x,y,3, 130, 255)
             uh.show()
-    time.sleep(1)
-    t -= 1
+        time.sleep(1)
+        t -= 1
