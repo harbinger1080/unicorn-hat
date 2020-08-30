e93dbe70abe4a7936ca226f3954ca24e79ddd44f
harbinger1080 <harbinger1080@penguin>
2020-08-30 12:07:28 -0400

    trying that again

--- justin/rgbtohsv.py
+++ justin/rgbtohsv.py
@@ -1,4 +1,7 @@
 import colorsys as col
+import time
+
+t = 60
 
 def set_pixel_hsv(x, y, h, s, v):
     r, g, b = [int(c*255) for c in col.hsv_to_rgb(h,s,v)]
@@ -7,10 +10,9 @@ def set_pixel_hsv(x, y, h, s, v):
 def colordots(ldots):
     if (ldots % 1 == 0):
             sat = 1.0
-        else:    
-            sat = (1.0 * (ldots % 1))
-        set_pixel_hsv(.58, sat, 1.0)
-    else:
+    else:    
+        sat = (1.0 * (ldots % 1))
+    set_pixel_hsv(0, 0, .58, sat, 1.0)
 
 
 while (t):
@@ -19,7 +21,7 @@ while (t):
     #print(mins,secs)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
-    lights = t/dotst
+    lights = t/15
     colordots(lights)
     time.sleep(1)
     t -= 1
