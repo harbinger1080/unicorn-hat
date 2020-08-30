e1205036f94f9d16df253712e6caeaa911bd763b
harbinger1080 <harbinger1080@penguin>
2020-08-29 20:45:31 -0400

    more columns

--- justin/lights.py
+++ justin/lights.py
@@ -12,11 +12,13 @@ t = int(input("How many seconds? "))
 def dcounter(totaldots):
     width = math.ceil(totaldots/4)
     for x in range(width):
+        print(x)
         if (totaldots > 4):
             dots = 4
         else:
             dots = totaldots
         for y in range(4):
+            print(y)
             if (y < dots):
                 uh.set_pixel(x,y, 3, 130, 255)
             else:
@@ -27,7 +29,7 @@ def dcounter(totaldots):
 while (t):
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
-    print(timeformat, end='\r')
+    #print(timeformat, end='\r')
     lremaining = t//15
     if (lremaining <= 1):
         lremaining = 1
