dce31eefb1d5cc2edbea1a95bcd1dd8a6cdf8ee6
harbinger1080 <harbinger1080@penguin>
2020-08-29 20:38:18 -0400

    figuring it out

--- justin/lights.py
+++ justin/lights.py
@@ -28,6 +28,9 @@ while (t):
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
     print(timeformat, end='\r')
-    dcounter(t//15)
+    lremaining = t//15
+    if (lremaining <= 15):
+        lremaining = 15
+    dcounter(lremaining)
     time.sleep(1)
     t -= 1
